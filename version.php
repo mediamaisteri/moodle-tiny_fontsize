78bf467dc57f4973b8392588d948039769793c98
Mikko Haiku <mikko.haiku@mediamaisteri.com>
2023-09-18 14:43:29 +0300

    Bump release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_fontsize';
-$plugin->release = '0.1.0';
+$plugin->release = '0.1.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022112800;
 $plugin->version = 2023091800;
