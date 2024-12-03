cb91473bb943ad688a69d8884f79cc3f201823ac
Mikko Haiku <mikko.haiku@mediamaisteri.com>
2024-12-03 15:24:31 +0200

    Add icon to toolbar, too.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_fontsize';
-$plugin->release = '0.1.2';
+$plugin->release = '0.1.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2022112800;
-$plugin->version = 2024082700;
+$plugin->version = 2024120300;
